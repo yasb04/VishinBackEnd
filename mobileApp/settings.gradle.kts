cdd5540974169e60165ddbd05a5506705fbf3e3e
Potatisgrottan <olleolheim@gmail.com>
2026-01-25 13:42:16 +0100

    mobileApp strukturen

---
+++ mobileApp/settings.gradle.kts
@@ -0,0 +1,23 @@
+﻿pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mobileApplication"
+include(":app")
